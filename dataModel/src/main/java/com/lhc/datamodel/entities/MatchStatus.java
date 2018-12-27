9fb0fe629d02796c6534903de7f5c51d47920fba
GauthierRobert <40065728+GauthierRobert@users.noreply.github.com>
2018-12-27 15:35:34 +0100

    Update MatchStatus.java

--- dataModel/src/main/java/com/lhc/datamodel/entities/MatchStatus.java
+++ dataModel/src/main/java/com/lhc/datamodel/entities/MatchStatus.java
@@ -1,1 +1,21 @@
 
+package com.lhc.datamodel.entities;
+
+public enum MatchStatus {
+
+    OPEN(1),
+    CLOSED(2);
+
+    private int value;
+
+    VoteType(int value) {
+        this.value = value;
+
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+
+}
