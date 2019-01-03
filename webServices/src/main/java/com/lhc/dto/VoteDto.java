29767788a4fa212cf8a4aedba4640863f1da6e92
GauthierRobert <40065728+GauthierRobert@users.noreply.github.com>
2019-01-03 11:44:44 +0100

    Update VoteDto.java

--- webServices/src/main/java/com/lhc/dto/VoteDto.java
+++ webServices/src/main/java/com/lhc/dto/VoteDto.java
@@ -7,6 +7,12 @@ import lombok.Data;
 public class VoteDto {
 
     private String reference;
+    
+    private String ballot_ref;
+    
+    private String match_ref;
+    
+    private String competition_ref;
 
     // 1 for first top vote
     // 2 for second top vote
